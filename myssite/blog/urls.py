f69a30cead1907729108ac20578feec045228c5f
Roz-Alex <90191426+Roz-Alex@users.noreply.github.com>
2022-06-14 16:33:30 +0300

    Add files via upload

---
+++ myssite/blog/urls.py
@@ -0,0 +1,20 @@
+from . import views
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('search', views.search, name='search'),
+    path('summernote/', include('django_summernote.urls')),
+
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+#path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
